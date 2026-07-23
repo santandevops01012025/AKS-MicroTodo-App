97d569b8dc9c220b281d7eca00c31243bd3bb68e
santandevops01012025 <santandevops01012025@gmail.com>
2026-07-23 08:22:33 +0000

    Update app.py

--- GetTasksTodoMicroservice/app.py
+++ GetTasksTodoMicroservice/app.py
@@ -6,7 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 
 # Fetch the connection string from the environment variable
-connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:meharserver123.database.windows.net,1433;Database=mehardb;Uid=Useradmin;Pwd=Password@12345;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+connection_string = os.environ.get("CONNECTION_STRING")
 
 # Check if the connection string is available
 if connection_string:
